47fba70077b47d048f08714dd43e71acae4f6580
thiago souza <thiagosouzadasilva78@gmail.com>
2026-05-07 18:34:23 -0300

    fix: add _PUBLIC na variaveis de ambiente

--- src/data/public/getPosts.ts
+++ src/data/public/getPosts.ts
@@ -3,7 +3,7 @@ import { PostsAreaType } from "@/types/postsAreaType";
 import axios from "axios";
 
 export const getPosts = async (pages: number):Promise<PostsAreaType | null> => {
-const apiUrl = process.env.NEXT_URL_PUBLIC;
+const apiUrl = process.env.NEXT_PUBLIC_URL_PUBLIC;
   try {
     const response = await axios.get(`${apiUrl}}/feed/${pages || 1}`);
     //console.log("response do getPost",response.data);
