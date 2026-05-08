09d3cea32b35ba4c38d15de8824513956b3628a5
thiago souza <thiagosouzadasilva78@gmail.com>
2026-05-08 11:32:54 -0300

    ref: linhas removidas

--- src/proxy.ts
+++ src/proxy.ts
@@ -3,8 +3,7 @@ import { NextRequest, NextResponse } from "next/server";
 export const proxy = (req: NextRequest) => {
   const token = req.cookies.get("token")?.value;
 
-  /*const isPrivate = req.nextUrl.pathname.startsWith("/user") 
-  || req.nextUrl.pathname.startsWith("/post");*/
+  
 
   if( !token) {
     return NextResponse.redirect(new URL("/login", req.url));
